64755e36bce8cbb9af0a4f4cdfc2c2fd3b2fd4a1
r0b0bcb <36454538+r0b0bcb@users.noreply.github.com>
2018-04-22 13:23:27 -0400

    Update case_study_2.py

--- case_study_2.py
+++ case_study_2.py
@@ -15,3 +15,33 @@ for language in book_titles:
                 text = read_book(inputfile)
                 hamlets.loc[title_num] = language, text
                 title_num += 1
+
+# 2: Create Pandas Dataframe to record words and their useage frequency
+
+language, text = hamlets.iloc[0]
+counted_text = count_words_fast(text)
+data = pd.DataFrame({"word": list(counted_text.keys()), "count": list(counted_text.values())})
+
+# 3: Add two more columns to previous dataframe; one for word length and other for frequency qualifier
+
+data["length"] = data['word'].str.len()
+def frequency(count):
+    if count == 1:
+        return "unique"
+    elif count > 10:
+         return "frequent"
+    else:
+         return "infrequent"
+data["frequency"] = data['count'].apply(frequency)
+
+# 4: New dataframe that pulls data from previous dataframe
+
+sub_data = pd.DataFrame({
+    "language" : language,
+    "frequency" : ["frequent", "infrequent", "unique"],
+    "mean_word_length" : data.groupby(by = "frequency")["length"].mean(),
+    "num_words" : data.groupby(by = "frequency")["count"].size()
+})
+
+# 5: Above code is called summarize_text & returns sub_data. 
+
