7daa8bccd01ed83a30dfd8325ea9545fb4f10318
r0b0bcb <36454538+r0b0bcb@users.noreply.github.com>
2018-04-22 13:42:02 -0400

    Update case_study_2.py

--- case_study_2.py
+++ case_study_2.py
@@ -45,3 +45,45 @@ sub_data = pd.DataFrame({
 
 # 5: Above code is called summarize_text & returns sub_data. 
 
+grouped_data = pd.DataFrame(columns = ["language", "frequency", "mean_word_length", "num_words"])
+# I used "grouped_data=pd.DataFrame({})"
+for loop in range (hamlets.shape[0]): #and "(0, len(hamlets))" which gave me an identical output, but datacamp didn't like it.
+    language, text = hamlets.iloc[loop]
+    sub_data = summarize_text(language, text)
+    grouped_data = grouped_data.append(sub_data)
+   
+# 6: Plot the stats
+
+colors = {"Portuguese": "green", "English": "blue", "German": "red"}
+markers = {"frequent": "o","infrequent": "s", "unique": "^"}
+import matplotlib.pyplot as plt
+for i in range(grouped_data.shape[0]):
+    row = grouped_data.iloc[i]
+    plt.plot(row.mean_word_length, row.num_words,
+        marker=markers[row.frequency],
+        color = colors[row.language],
+        markersize = 10
+    )
+
+color_legend = []
+marker_legend = []
+for color in colors:
+    color_legend.append(
+        plt.plot([], [],
+        color=colors[color],
+        marker="o",
+        label = color, markersize = 10, linestyle="None")
+    )
+for marker in markers:
+    marker_legend.append(
+        plt.plot([], [],
+        color="k",
+        marker=markers[marker],
+        label = marker, markersize = 10, linestyle="None")
+    )
+plt.legend(numpoints=1, loc = "upper left")
+
+plt.xlabel("Mean Word Length")
+plt.ylabel("Number of Words")
+plt.show()
+
